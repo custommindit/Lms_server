15c24a6c28312fc5f5c4e7a30d24f11c9fd852e7
Mahmoud Eldeeb <116121838+Eldeeb71@users.noreply.github.com>
2023-09-05 15:56:14 +0300

    post scheema

---
+++ src/models/posts.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+  {
+    user_image: {
+      type: String,
+      required: true,
+    },
+    user_name: {
+      type: String,
+      required: true,
+    },
+    user_email: {
+      type: String,
+      required: true,
+    },
+    text: String,
+    level: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    comments: [
+      {
+        text: String,
+        user_image: {
+          type: String,
+          required: true,
+        },
+        user_name: {
+          type: String,
+          required: true,
+        },
+        user_email: {
+          type: String,
+          required: true,
+        },
+        replies: [
+          {
+            text: String,
+            user_image: {
+              type: String,
+              required: true,
+            },
+            user_name: {
+              type: String,
+              required: true,
+            },
+            user_email: {
+              type: String,
+              required: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+module.exports = Post;
