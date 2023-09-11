b8abc20dd31f0b9141f27839c9135a04e26a6936
Mahmoud Eldeeb <116121838+Eldeeb71@users.noreply.github.com>
2023-09-11 09:56:14 +0300

    fix

--- src/routes/api/studentrouter.js
+++ src/routes/api/studentrouter.js
@@ -1,8 +1,9 @@
 const studentcontroller=require('../../controllers/studentcontroller')
 const router= require("express").Router();
+const{checkToken}=require('../../auth/token_validation')
 
 router.post('/sign_up',studentcontroller.signup)
 router.post('/login',studentcontroller.login)
-router.get('/personal',studentcontroller.getdata)
+router.get('/personal',checkToken,studentcontroller.getdata)
 
 module.exports=router
