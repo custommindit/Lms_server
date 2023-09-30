1180703debb981ff11c7e8cf24d1aea3b9a3cd47
Ehab Atef Mahmoud <73141172+ehabatef205@users.noreply.github.com>
2023-09-30 09:41:21 +0300

    system

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,34 +1,37 @@
-const router= require("express").Router();
-const studentrouter=require('./api/studentrouter')
-const unitrouter=require('./api/unitrouter')
-const sectionrouter=require('./api/sectionrouter')
-const quizrouter=require('./api/quizrouter')
-const examrouter=require('./api/examrouter')
-const adminrouter=require('./api/adminrouter')
-const materialrouter=require('./api/materialrouter')
-const postrouter=require('./api/postrouter')
-const adminpostrouter=require('./api/adminpostrouter')
-const announcementrouter=require('./api/announcementrouter')
+const router = require("express").Router();
+const studentrouter = require('./api/studentrouter')
+const unitrouter = require('./api/unitrouter')
+const sectionrouter = require('./api/sectionrouter')
+const quizrouter = require('./api/quizrouter')
+const examrouter = require('./api/examrouter')
+const adminrouter = require('./api/adminrouter')
+const materialrouter = require('./api/materialrouter')
+const postrouter = require('./api/postrouter')
+const adminpostrouter = require('./api/adminpostrouter')
+const announcementrouter = require('./api/announcementrouter')
+const systemrouter = require('./api/systemrouter')
 
 
-router.use('/student',studentrouter)
+router.use('/student', studentrouter)
 
-router.use('/unit',unitrouter)
+router.use('/unit', unitrouter)
 
-router.use('/section',sectionrouter)
+router.use('/section', sectionrouter)
 
-router.use('/quiz',quizrouter)
+router.use('/quiz', quizrouter)
 
-router.use('/exam',examrouter)
+router.use('/exam', examrouter)
 
-router.use('/admin',adminrouter)
+router.use('/admin', adminrouter)
 
-router.use('/material',materialrouter)
+router.use('/material', materialrouter)
 
-router.use('/post',postrouter)
+router.use('/post', postrouter)
 
-router.use('/adminpost',adminpostrouter)
+router.use('/adminpost', adminpostrouter)
 
-router.use('/announcement',announcementrouter)
+router.use('/announcement', announcementrouter)
 
-module.exports=router
+router.use('/system', systemrouter)
+
+module.exports = router
