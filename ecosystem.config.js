271894185060bdc0b2bc4b165ca122cab8439679
custommindit <custommindit@gmail.com>
2025-06-29 17:19:40 +0000

    changes from server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,18 @@
 module.exports = {
-    apps : [{
-      name   : "lms",
-      script : "./index.js"
-    }]
-  }
+  apps : [{
+    name   : "hest",
+    script : "./index.js",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '500M',
+    env: {
+      NODE_ENV: 'production',
+      TIMEOUT: '1200000', // 20 minutes in milliseconds
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      TIMEOUT: '1200000', // 20 minutes in milliseconds
+    },
+  }]
+}
