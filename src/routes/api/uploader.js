d613670ee1b311c59ca4d1ba30b28640fc50fb2c
abdullah-taha5 <abdallahtaha493@gmail.com>
2025-08-17 14:16:57 +0300

    change limits in multer

--- src/routes/api/uploader.js
+++ src/routes/api/uploader.js
@@ -1,17 +1,17 @@
-const multer = require('multer');
+const multer = require("multer");
 
 // Set up the multer storage configuration
 const storage = multer.diskStorage({
-    limits: { fileSize: 600 * 1024 * 1024 }, 
-    destination: function (req, file, cb) {
-        cb(null, './uploads'); // Specify the destination folder for uploaded images
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + file.originalname); // Use the current timestamp to generate a unique filename
-    }
+  destination: function (req, file, cb) {
+    cb(null, "./uploads"); // Specify the destination folder for uploaded images
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname); // Use the current timestamp to generate a unique filename
+  },
 });
 
-const upload = multer({ 
-    storage: storage
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 * 1024 },
 });
-module.exports={upload};
+module.exports = { upload };
